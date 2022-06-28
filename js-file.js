afcbbf1be6c5ca867f8b9c5e21c1c9bff4391bb2
Binh <lethanhbinh6203@gmail.com>
2022-06-28 08:55:44 +0700

    Set limit for alpha change

--- js-file.js
+++ js-file.js
@@ -17,21 +17,29 @@ function mouseOver() {
     divs.forEach((div) => {
         div.addEventListener('mouseover', () => {
             const bg = div.style.background;
-            let alpha = +bg.slice(14, 17);      
-            alpha += 0.1;
-            div.style.background = `rgba(0,0,0,${alpha})`;
+            
+            if (!(bg == 'rgb(0, 0, 0)')) {
+                let alpha = 0.1 + +bg.slice(14, 17);  
+                div.style.background = `rgba(0,0,0,${alpha})`;  
+            }
         })   
     })
 }
 
 const clearBtn = document.getElementById('clearBtn');
 clearBtn.addEventListener('click', () => {
+    console.clear();
+
     while (container.firstChild) {
         container.removeChild(container.lastChild);
     }
     let px = prompt('How much squares per side do you want?');
+    
     while (!(px > 0 && px < 100)) {
-        px = prompt('Invalid number! Try again:');
+        if (px == '') {
+            px = 16;
+        }
+        else px = prompt('Invalid number! Try again:');
     }
     makeGrid(px);
 })
